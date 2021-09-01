5654e299951c9752737a390cc824c588523e5c6b
khunemz <khunemz@gmail.com>
2021-09-01 21:59:06 +0700

    add banner category

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,43 +5,109 @@
 
 <section class="main-content" style="padding-top: 10px;">
 
-  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <!-- START: Carousel -->
+  <div id="mainCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#mainCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#mainCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#mainCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="{{ asset('img/c_1_1.jpg') }}" class="d-block w-100 img-carousel" alt="...">
-        <div class="carousel-caption d-none d-md-block">
+        <img src="{{ asset('img/c_1_1.png') }}" class="d-block w-100 carousel-item-img" alt="...">
+        {{-- <div class="carousel-caption d-none d-md-block">
           <h5>First slide label</h5>
           <p>Some representative placeholder content for the first slide.</p>
-        </div>
+        </div> --}}
       </div>
       <div class="carousel-item">
-        <img src="{{ asset('img/c_1_2.jpg') }}" class="d-block w-100 img-carousel" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
-        </div>
+        <img src="{{ asset('img/c_1_2.png') }}" class="d-block w-100 carousel-item-img" alt="...">
+       
       </div>
       <div class="carousel-item">
-        <img src="{{ asset('img/c_1_3.jpg') }}" class="d-block w-100 img-carousel" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
-        </div>
+        <img src="{{ asset('img/c_1_3.png') }}" class="d-block w-100 carousel-item-img" alt="...">
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#mainCarousel" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#mainCarousel" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
+  </div>
+
+  <!-- END: Carousel -->
+
+  <div class="card category-card">
+    <div class="card-body">
+      <div class="row">
+        <div class="category-group">
+          <div class="category-item">
+            <div class="d-block">
+              <img 
+              src="https://salt.tikicdn.com/cache/w100/ts/upload/31/d1/ad/8f8589ccf8a4ce2761ad51bae11eb4a6.jpg" 
+              class="d-block img-fluid"
+              width="100" height="100"
+              />
+            </div>
+            <div class="category-desc text-center">
+              <a href="#" class="text-decoration-none">Category # 1</a>
+            </div>
+          </div>
+          <div class="category-item">
+            <div class="d-block">
+              <img 
+              src="https://salt.tikicdn.com/cache/w100/ts/upload/73/e0/7d/af993bdbf150763f3352ffa79e6a7117.png" 
+              class="d-block img-fluid"
+              width="100" height="100"
+              />
+            </div>
+            <div class="category-desc text-center">
+              <a href="#" class="text-decoration-none">Category # 2</a>
+            </div>
+          </div>
+          <div class="category-item">
+            <div class="d-block">
+              <img 
+              src="https://salt.tikicdn.com/cache/w100/ts/upload/4a/b2/c5/b388ee0e511889c83fab1217608fe82f.png" 
+              class="d-block img-fluid"
+              width="100" height="100"
+              />
+            </div>
+            <div class="category-desc text-center">
+              <a href="#" class="text-decoration-none">Category # 3</a>
+            </div>
+          </div>
+          <div class="category-item">
+            <div class="d-block">
+              <img 
+              src="https://salt.tikicdn.com/cache/w100/ts/upload/a0/0d/90/bab67b6da67117f40538fc54fb2dcb5e.png" 
+              class="d-block img-fluid"
+              width="100" height="100"
+              />
+            </div>
+            <div class="category-desc text-center">
+              <a href="#" class="text-decoration-none">Category # 4</a>
+            </div>
+          </div>
+          <div class="category-item">
+            <div class="d-block">
+              <img 
+              src="https://salt.tikicdn.com/cache/w100/ts/upload/a9/58/39/872e4acbdb3576be53b57c05a386860b.png" 
+              class="d-block img-fluid"
+              width="100" height="100"
+              />
+            </div>
+            <div class="category-desc text-center">
+              <a href="#" class="text-decoration-none">Category # 1</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
   </div>
 
 </section>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,8 +14,8 @@
   <link rel="manifest" href="site.webmanifest">
   <link rel="apple-touch-icon" href="icon.png"> 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link href="css/app.css"/>
-  <link href="css/styles.css"/>
+  <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+  <link href="css/app.css" rel="stylesheet" type="text/css"/>
   <meta name="theme-color" content="#fafafa">
 </head>
 
@@ -37,25 +37,36 @@
       <div class="collapse navbar-collapse" id="bdNavbar">
         <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav pt-2 py-md-0">
           <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2" href="/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Bootstrap');">Home</a>
+            <a class="nav-link p-2" href="/" 
+            onclick="ga('send', 'event', 'Navbar', 'Community links', 'Bootstrap');">หน้าหลัก</a>
           </li>
           <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2 active" aria-current="true" href="/docs/5.0/getting-started/introduction/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">Docs</a>
+            <a class="nav-link p-2 active" aria-current="true" 
+            href="/docs/5.0/getting-started/introduction/" 
+            onclick="ga('send', 'event', 'Navbar', 'Community links', 'Docs');">สินค้าทั้งหมด</a>
           </li>
           <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2" href="/docs/5.0/examples/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Examples');">Examples</a>
+            <a class="nav-link p-2" href="/docs/5.0/examples/" 
+            onclick="ga('send', 'event', 'Navbar', 'Community links', 'Examples');">ติดตามสถานะ</a>
           </li>
-          <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2" href="https://icons.getbootstrap.com/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Icons');" target="_blank" rel="noopener">Icons</a>
-          </li>
-          <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2" href="https://themes.getbootstrap.com/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Themes');" target="_blank" rel="noopener">Themes</a>
-          </li>
-          <li class="nav-item col-6 col-md-auto">
-            <a class="nav-link p-2" href="https://blog.getbootstrap.com/" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Blog');" target="_blank" rel="noopener">Blog</a>
-          </li>
+         
         </ul>  
         <hr class="d-md-none text-white-50">           
+        <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
+          
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+            </a>
+            <div class="dropdown-menu account-dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Setting</a>
+              <a class="dropdown-item" href="#">Profile</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Logout</a>
+            </div>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -67,7 +78,7 @@
     @yield('content')
   </div>
   <!-- END: content -->
-  
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   <script src="js/app.js"></script>
