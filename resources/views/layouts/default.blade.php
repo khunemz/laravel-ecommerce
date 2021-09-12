450e502ee33db737b4db287439fc12c445d4f1f5
khunemz <khunemz@gmail.com>
2021-09-12 20:51:28 +0700

    change redirect

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -66,10 +66,9 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
             </a>
             <div class="dropdown-menu account-dropdown-menu" aria-labelledby="navbarDropdown">
-
               @auth              
               <a class="dropdown-item" href="javascript:void(0)">
-                {{ auth()->user()->name }}
+                Logged in as {{ auth()->user()->name }}
               </a>
               <a class="dropdown-item">
                 <form action="{{ url('logout') }}" method="POST">
